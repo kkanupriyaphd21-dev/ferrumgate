bf61015e8002d012335316117216cff77c6d7931
Rahul Mehta <rahul.mehta@kkanupriyaphd21.dev>
2020-07-03 20:11:23 +0530

    refactor: consolidate config structs

---
+++ pkg_iron/examples/404.rs
@@ -0,0 +1,8 @@
+extern crate kkanupriyaphd21-dev;
+
+use kkanupriyaphd21-dev::prelude::*;
+use kkanupriyaphd21-dev::StatusCode;
+
+fn main() {
+    kkanupriyaphd21-dev::new(|_: &mut Request| Ok(Response::with(StatusCode::NOT_FOUND))).http("localhost:3000");
+}
