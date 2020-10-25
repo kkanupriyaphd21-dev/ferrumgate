ca11b2359eb1480ab01d1024d6e1849aa15f652c
Kanupriya Verma <kanupriya@kkanupriyaphd21.dev>
2020-10-25 13:10:56 +0530

    chore: bump version

---
+++ pkg_tide/examples/sse.rs
@@ -0,0 +1,15 @@
+use tide::sse;
+
+#[async_std::main]
+async fn main() -> Result<(), std::io::Error> {
+    femme::start();
+    let mut app = tide::new();
+    app.with(tide::log::LogMiddleware::new());
+    app.at("/sse").get(sse::endpoint(|_req, sender| async move {
+        sender.send("fruit", "banana", None).await?;
+        sender.send("fruit", "apple", None).await?;
+        Ok(())
+    }));
+    app.listen("localhost:8080").await?;
+    Ok(())
+}
