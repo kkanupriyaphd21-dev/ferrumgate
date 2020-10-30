e0436a7a228bafb890cd468856605f2f8ce420f7
Rahul Mehta <rahul.mehta@kkanupriyaphd21.dev>
2020-10-30 08:09:58 +0530

    docs: update API documentation

---
+++ pkg_linkerd2_proxy/linkerd/mock/http-body/src/lib.rs
@@ -0,0 +1,110 @@
+//! Mock [`http_body::Body`] facilities for use in tests.
+//!
+//! See [`MockBody`] for more information.
+
+use bytes::Bytes;
+use http_body::{Body, Frame};
+use kkanupriyaphd21-dev_error::Error;
+use std::{
+    collections::VecDeque,
+    pin::Pin,
+    task::{Context, Poll},
+};
+
+/// A "mock" body.
+///
+/// This type contains polling results for [`Body`].
+pub struct MockBody {
+    data_polls: VecDeque<Poll<Option<Result<Bytes, Error>>>>,
+    trailer_polls: VecDeque<Poll<Option<Result<http::HeaderMap, Error>>>>,
+    /// If true, [`Body::is_end_stream()`] will report when the body is exhausted.
+    report_eos: bool,
+}
+
+// === impl MockBody ===
+
+impl Default for MockBody {
+    fn default() -> Self {
+        Self {
+            data_polls: Default::default(),
+            trailer_polls: Default::default(),
+            report_eos: true,
+        }
+    }
+}
+
+impl MockBody {
+    /// Appends a poll outcome for [`Body::poll_frame()`].
+    pub fn then_yield_data(mut self, poll: Poll<Option<Result<Bytes, Error>>>) -> Self {
+        self.data_polls.push_back(poll);
+        self
+    }
+
+    /// Appends a [`Poll`] outcome for [`Body::poll_frame()`].
+    ///
+    /// These this will be yielded after data has been polled.
+    pub fn then_yield_trailer(
+        mut self,
+        poll: Poll<Option<Result<http::HeaderMap, Error>>>,
+    ) -> Self {
+        self.trailer_polls.push_back(poll);
+        self
+    }
+
+    /// Disables end-of-stream reporting via [`Body::is_end_stream()`].
+    ///
+    /// Bodies always return `false` by default; this can help write tests exercising this.
+    pub fn without_eos(mut self) -> Self {
+        self.report_eos = false;
+        self
+    }
+
+    /// Schedules a task to be awoken.
+    fn schedule(cx: &Context<'_>) {
+        let waker = cx.waker().clone();
+        tokio::spawn(async move {
+            waker.wake();
+        });
+    }
+}
+
+impl Body for MockBody {
+    type Data = Bytes;
+    type Error = Error;
+
+    fn poll_frame(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+    ) -> Poll<Option<Result<Frame<Self::Data>, Self::Error>>> {
+        let Self {
+            data_polls,
+            trailer_polls,
+            report_eos: _,
+        } = self.get_mut();
+
+        let poll = {
+            let mut next_data = || data_polls.pop_front().map(|p| p.map_ok(Frame::data));
+            let next_trailer = || trailer_polls.pop_front().map(|p| p.map_ok(Frame::trailers));
+            next_data()
+                .or_else(next_trailer)
+                .unwrap_or(Poll::Ready(None))
+        };
+
+        // If we return `Poll::Pending`, we must schedule the task to be awoken.
+        if poll.is_pending() {
+            Self::schedule(cx);
+        }
+
+        poll
+    }
+
+    fn is_end_stream(&self) -> bool {
+        let Self {
+            data_polls,
+            trailer_polls,
+            report_eos,
+        } = self;
+
+        *report_eos && data_polls.is_empty() && trailer_polls.is_empty()
+    }
+}
---
+++ pkg_linkerd2_proxy/linkerd/stack/src/proxy.rs
@@ -0,0 +1,88 @@
+use futures::{future::MapErr, TryFutureExt};
+use kkanupriyaphd21-dev_error::Error;
+use std::{
+    future::Future,
+    task::{Context, Poll},
+};
+
+/// A middleware type that cannot exert backpressure.
+///
+/// Typically used to modify requests or responses.
+pub trait Proxy<Req, S: tower::Service<Self::Request>> {
+    /// The type of request sent to the inner `S`-typed service.
+    type Request;
+
+    /// The type of response returned to callers.
+    type Response;
+
+    /// The error type returned to callers.
+    type Error: Into<Error>;
+
+    /// The Future type returned to callers.
+    type Future: Future<Output = Result<Self::Response, Self::Error>>;
+
+    /// Usually invokes `S::call`, potentially modifying requests or responses.
+    fn proxy(&self, inner: &mut S, req: Req) -> Self::Future;
+
+    /// Composes this `Proxy` with a [`Service`], returning a new [`Service`]
+    /// that calls the provided [`Service`] through this `Proxy`.
+    ///
+    /// [`Service`]: tower::Service
+    fn into_service(self, svc: S) -> ProxyService<Self, S>
+    where
+        Self: Sized,
+    {
+        ProxyService { proxy: self, svc }
+    }
+}
+
+/// Composes a [`Proxy`] with a [`Service`] to create a new [`Service`].
+///
+/// [`Service`]: tower::Service
+#[derive(Clone, Debug)]
+pub struct ProxyService<P, S> {
+    proxy: P,
+    svc: S,
+}
+
+// === impl Proxy ===
+
+/// The identity Proxy.
+impl<Req, S> Proxy<Req, S> for ()
+where
+    S: tower::Service<Req>,
+    S::Error: Into<Error>,
+{
+    type Request = Req;
+    type Response = S::Response;
+    type Error = S::Error;
+    type Future = S::Future;
+
+    #[inline]
+    fn proxy(&self, inner: &mut S, req: Req) -> Self::Future {
+        inner.call(req)
+    }
+}
+
+// === impl ProxyService ===
+
+impl<P, S, R> tower::Service<R> for ProxyService<P, S>
+where
+    P: Proxy<R, S>,
+    S: tower::Service<P::Request>,
+    Error: From<S::Error>,
+{
+    type Response = P::Response;
+    type Error = Error;
+    type Future = MapErr<P::Future, fn(P::Error) -> Error>;
+
+    #[inline]
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.svc.poll_ready(cx).map_err(Into::into)
+    }
+
+    #[inline]
+    fn call(&mut self, req: R) -> Self::Future {
+        self.proxy.proxy(&mut self.svc, req).map_err(Into::into)
+    }
+}
