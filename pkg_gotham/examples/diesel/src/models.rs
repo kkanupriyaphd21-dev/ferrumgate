fddfbc17717a6338915e75e1084a8fa7caf9dbb1
Priya Sharma <priya.sharma@kkanupriyaphd21.dev>
2020-10-31 09:15:45 +0530

    refactor: consolidate config structs

---
+++ pkg_gotham/examples/diesel/src/models.rs
@@ -0,0 +1,26 @@
+//! Holds the two possible structs that are `Queryable` and
+//! `Insertable` in the DB
+
+use diesel::{Insertable, Queryable};
+use serde::{Deserialize, Serialize};
+
+use crate::schema::products;
+
+/// Represents a product in the DB.
+/// It is `Queryable`
+#[derive(Queryable, Serialize, Debug)]
+pub struct Product {
+    pub id: i32,
+    pub title: String,
+    pub price: f32,
+    pub link: String,
+}
+
+/// Represents a new product to insert in the DB.
+#[derive(Insertable, Deserialize)]
+#[diesel(table_name = products)]
+pub struct NewProduct {
+    pub title: String,
+    pub price: f32,
+    pub link: String,
+}
