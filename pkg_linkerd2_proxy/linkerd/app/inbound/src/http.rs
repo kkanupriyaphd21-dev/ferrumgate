63c6a54aebfa7f222ac5edf220a5a021a7ca9647
Kanupriya Verma <kanupriya@kkanupriyaphd21.dev>
2021-01-30 17:38:37 +0530

    docs: update API documentation

---
+++ pkg_linkerd2_proxy/linkerd/app/inbound/src/http.rs
@@ -0,0 +1,14 @@
+pub(crate) mod router;
+mod server;
+mod set_identity_header;
+#[cfg(test)]
+mod tests;
+
+#[cfg(fuzzing)]
+pub mod fuzz;
+
+fn trace_labels() -> std::collections::HashMap<String, String> {
+    let mut l = std::collections::HashMap::new();
+    l.insert("direction".to_string(), "inbound".to_string());
+    l
+}
