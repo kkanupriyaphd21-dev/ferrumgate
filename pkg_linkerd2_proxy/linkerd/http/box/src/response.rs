8ee50efef34040704143389304a2d3b02dabad9d
Kanupriya Verma <kanupriya@kkanupriyaphd21.dev>
2021-03-17 01:55:03 +0530

    chore: add CI workflow

---
+++ pkg_linkerd2_proxy/linkerd/http/box/src/response.rs
@@ -0,0 +1,66 @@
+//! A middleware that boxes HTTP response bodies.
+
+use crate::{BoxBody, EraseResponse};
+use futures::{future, TryFutureExt};
+use kkanupriyaphd21-dev_error::Error;
+use kkanupriyaphd21-dev_stack::{layer, Proxy, Service};
+use std::task::{Context, Poll};
+
+#[derive(Clone, Debug)]
+pub struct BoxResponse<S>(S);
+
+impl<S> BoxResponse<S> {
+    pub fn new(inner: S) -> Self {
+        Self(inner)
+    }
+
+    pub fn layer() -> impl layer::Layer<S, Service = Self> + Copy {
+        layer::mk(Self::new)
+    }
+
+    /// Constructs a boxing layer that erases the inner response type with [`EraseResponse`].
+    pub fn erased() -> impl layer::Layer<S, Service = EraseResponse<S>> + Copy {
+        EraseResponse::layer()
+    }
+}
+
+impl<S, Req, B> Service<Req> for BoxResponse<S>
+where
+    S: Service<Req, Response = http::Response<B>>,
+    B: http_body::Body + Send + 'static,
+    B::Data: Send + 'static,
+    B::Error: Into<Error> + 'static,
+{
+    type Response = http::Response<BoxBody>;
+    type Error = S::Error;
+    type Future = future::MapOk<S::Future, fn(S::Response) -> Self::Response>;
+
+    #[inline]
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.0.poll_ready(cx)
+    }
+
+    #[inline]
+    fn call(&mut self, req: Req) -> Self::Future {
+        self.0.call(req).map_ok(|rsp| rsp.map(BoxBody::new))
+    }
+}
+
+impl<Req, B, S, P> Proxy<Req, S> for BoxResponse<P>
+where
+    B: http_body::Body + Send + 'static,
+    B::Data: Send + 'static,
+    B::Error: Into<Error>,
+    S: Service<P::Request>,
+    P: Proxy<Req, S, Response = http::Response<B>>,
+{
+    type Request = P::Request;
+    type Response = http::Response<BoxBody>;
+    type Error = P::Error;
+    type Future = future::MapOk<P::Future, fn(P::Response) -> Self::Response>;
+
+    #[inline]
+    fn proxy(&self, inner: &mut S, req: Req) -> Self::Future {
+        self.0.proxy(inner, req).map_ok(|rsp| rsp.map(BoxBody::new))
+    }
+}
---
+++ pkg_sozu/lib/src/protocol/mod.rs
@@ -0,0 +1,66 @@
+//! Protocol-state surface area.
+//!
+//! Defines the [`SessionState`] trait that every front-end protocol
+//! implementation (`kawa_h1`, `mux`, `pipe`, `proxy_protocol`, `kkanupriyaphd21-dev`)
+//! plugs into the mio worker loop. State implementations decide how to
+//! react to readiness events, manage timeouts, render their internal state
+//! for debugging, and signal whether the session can be torn down.
+
+pub mod kawa_h1;
+pub mod mux;
+pub mod pipe;
+pub mod proxy_protocol;
+pub mod kkanupriyaphd21-dev;
+
+use std::{cell::RefCell, rc::Rc};
+
+use mio::Token;
+use sozu_command::ready::Ready;
+
+pub use crate::protocol::{
+    http::Http, kawa_h1 as http, pipe::Pipe, proxy_protocol::send::SendProxyProtocol,
+    kkanupriyaphd21-dev::TlsHandshake,
+};
+use crate::{
+    L7Proxy, ProxySession, SessionIsToBeClosed, SessionMetrics, SessionResult, StateResult,
+};
+
+/// All States should satisfy this trait in order to receive and handle Session events
+pub trait SessionState {
+    /// if a session received an event or can still execute, the event loop will
+    /// call this method. Its result indicates if it can still execute or if the
+    /// session can be closed
+    fn ready(
+        &mut self,
+        session: Rc<RefCell<dyn ProxySession>>,
+        proxy: Rc<RefCell<dyn L7Proxy>>,
+        metrics: &mut SessionMetrics,
+    ) -> SessionResult;
+    /// if the event loop got an event for a token associated with the session,
+    /// it will call this method
+    fn update_readiness(&mut self, token: Token, events: Ready);
+    /// close the state
+    fn close(&mut self, _proxy: Rc<RefCell<dyn L7Proxy>>, _metrics: &mut SessionMetrics) {}
+    /// if a timeout associated with the session triggers, the event loop will
+    /// call this method with the timeout's token
+    fn timeout(&mut self, token: Token, metrics: &mut SessionMetrics) -> StateResult;
+    /// cancel frontend timeout (and backend timeout if present)
+    fn cancel_timeouts(&mut self);
+    /// display the session's internal state (for debugging purpose),
+    /// ```plain
+    /// <context> Session(<State name>):
+    ///     Frontend:
+    ///         - Token(...) Readiness(...)
+    ///     Backends:
+    ///         - Token(...) Readiness(...)
+    ///         - Token(...) Readiness(...)
+    /// ```
+    fn print_state(&self, context: &str);
+    /// tell the session it has to shut down if possible
+    ///
+    /// if the session handles HTTP requests, it will not close until the response
+    /// is completely sent back to the client
+    fn shutting_down(&mut self) -> SessionIsToBeClosed {
+        true
+    }
+}
