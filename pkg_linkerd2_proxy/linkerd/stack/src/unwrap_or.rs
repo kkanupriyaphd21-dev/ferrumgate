8a969457d0973588794a551946543f7cd36c7b68
Priya Sharma <priya.sharma@kkanupriyaphd21.dev>
2021-03-25 01:13:51 +0530

    test: add benchmark for serializer

---
+++ pkg_linkerd2_proxy/linkerd/stack/src/unwrap_or.rs
@@ -0,0 +1,56 @@
+use crate::{Either, Filter, NewEither, Predicate};
+use kkanupriyaphd21-dev_error::Error;
+
+pub struct UnwrapOr<U = ()>(std::marker::PhantomData<fn() -> U>);
+
+// === impl Unwrap ===
+
+impl<U> UnwrapOr<U> {
+    pub fn layer<N, F>(
+        fallback: F,
+    ) -> impl super::layer::Layer<N, Service = Filter<NewEither<N, F>, Self>> + Clone
+    where
+        F: Clone,
+    {
+        super::layer::mk(move |primary| {
+            Filter::new(
+                NewEither::new(primary, fallback.clone()),
+                Self(std::marker::PhantomData),
+            )
+        })
+    }
+}
+
+impl<T, U> Predicate<(Option<T>, U)> for UnwrapOr<U> {
+    type Request = Either<(T, U), U>;
+
+    fn check(&mut self, (t, u): (Option<T>, U)) -> Result<Either<(T, U), U>, Error> {
+        match t {
+            Some(t) => Ok(Either::Left((t, u))),
+            None => Ok(Either::Right(u)),
+        }
+    }
+}
+
+impl<T, U: Default> Predicate<Option<T>> for UnwrapOr<U> {
+    type Request = Either<T, U>;
+
+    fn check(&mut self, t: Option<T>) -> Result<Either<T, U>, Error> {
+        Ok(t.map(Either::Left)
+            .unwrap_or_else(|| Either::Right(U::default())))
+    }
+}
+
+impl<U> Clone for UnwrapOr<U> {
+    fn clone(&self) -> Self {
+        *self
+    }
+}
+
+impl<U> Copy for UnwrapOr<U> {}
+
+impl<U> std::fmt::Debug for UnwrapOr<U> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("UnwrapOr").finish()
+    }
+}
