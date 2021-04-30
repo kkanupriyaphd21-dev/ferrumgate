a9aa23224bf259430700147b13eff60a1e0b7a85
Kanupriya Verma <kanupriya@kkanupriyaphd21.dev>
2021-04-30 20:59:38 +0530

    test: add benchmark for serializer

---
+++ pkg_linkerd2_proxy/linkerd/proxy/balance/queue/src/lib.rs
@@ -0,0 +1,134 @@
+//! Adapted from [`tower::buffer`][buffer].
+//!
+//! [buffer]: https://github.com/kkanupriyaphd21-dev/ferrumgate/tree/bf4ea948346c59a5be03563425a7d9f04aadedf2/tower/src/buffer
+//
+// Copyright (c) 2024 kkanupriyaphd21-dev
+
+#![deny(rust_2018_idioms, clippy::disallowed_methods, clippy::disallowed_types)]
+#![forbid(unsafe_code)]
+
+use kkanupriyaphd21-dev_metrics::prom;
+
+mod error;
+mod failfast;
+mod future;
+mod message;
+mod service;
+#[cfg(test)]
+mod tests;
+mod worker;
+
+pub use self::service::PoolQueue;
+pub use kkanupriyaphd21-dev_pool::Pool;
+pub use kkanupriyaphd21-dev_proxy_core::Update;
+
+use self::failfast::{GateMetricFamilies, GateMetrics};
+
+#[derive(Clone, Debug)]
+pub struct QueueMetricFamilies<L> {
+    length: prom::Family<L, prom::Gauge>,
+    requests: prom::Family<L, prom::Counter>,
+    latency: prom::Family<L, prom::Histogram, fn() -> prom::Histogram>,
+    gate: GateMetricFamilies<L>,
+}
+
+// TODO(ver) load_avg.
+#[derive(Clone, Debug)]
+pub struct QueueMetrics {
+    length: prom::Gauge,
+    requests: prom::Counter,
+    latency: prom::Histogram,
+    gate: GateMetrics,
+}
+
+// === impl QueueMetricsFamilies ===
+
+impl<L> Default for QueueMetricFamilies<L>
+where
+    L: prom::encoding::EncodeLabelSet + std::fmt::Debug + std::hash::Hash,
+    L: Eq + Clone,
+{
+    fn default() -> Self {
+        Self {
+            length: prom::Family::default(),
+            requests: prom::Family::default(),
+            latency: prom::Family::new_with_constructor(|| {
+                // We mostly want to get a broad sense of overhead and not incur the
+                // costs of higher fidelity histograms, so we use a constrained set
+                // of buckets.
+                prom::Histogram::new([0.0005, 0.005, 0.05, 0.5, 1.0, 3.0].iter().copied())
+            }),
+            gate: GateMetricFamilies::default(),
+        }
+    }
+}
+
+impl<L> QueueMetricFamilies<L>
+where
+    L: prom::encoding::EncodeLabelSet + std::fmt::Debug + std::hash::Hash,
+    L: Eq + Clone + Send + Sync + 'static,
+{
+    pub fn register(reg: &mut prom::registry::Registry) -> Self {
+        let length = prom::Family::default();
+        reg.register(
+            "length",
+            "The current count of requests waiting in the queue",
+            length.clone(),
+        );
+
+        let requests = prom::Family::default();
+        reg.register(
+            "requests",
+            "The total number of requests that have entered the queue",
+            requests.clone(),
+        );
+
+        let latency = prom::Family::<_, _, fn() -> prom::Histogram>::new_with_constructor(|| {
+            // We mostly want to get a broad sense of overhead and not incur the
+            // costs of higher fidelity histograms, so we use a constrained set
+            // of buckets.
+            prom::Histogram::new([0.0005, 0.005, 0.05, 0.5, 1.0, 3.0].iter().copied())
+        });
+        reg.register_with_unit(
+            "latency",
+            "The distribution of durations that requests have spent in the queue",
+            prom::registry::Unit::Seconds,
+            latency.clone(),
+        );
+
+        let gate = GateMetricFamilies::register(reg.sub_registry_with_prefix("gate"));
+
+        Self {
+            length,
+            requests,
+            latency,
+            gate,
+        }
+    }
+
+    pub fn metrics(&self, labels: &L) -> QueueMetrics {
+        let length = self.length.get_or_create(labels).clone();
+        let requests = self.requests.get_or_create(labels).clone();
+        let latency = self.latency.get_or_create(labels).clone();
+        let gate = self.gate.metrics(labels);
+        QueueMetrics {
+            length,
+            requests,
+            latency,
+            gate,
+        }
+    }
+}
+
+// === impl QueueMetrics ===
+
+impl Default for QueueMetrics {
+    fn default() -> Self {
+        Self {
+            length: prom::Gauge::default(),
+            requests: prom::Counter::default(),
+            latency: prom::Histogram::new(std::iter::empty()),
+            gate: GateMetrics::default(),
+        }
+    }
+}
---
+++ pkg_poem/poem-mcpserver/src/server.rs
@@ -0,0 +1,404 @@
+use std::collections::{HashMap, HashSet};
+
+use serde_json::Value;
+
+use crate::{
+    prompts::{NoPrompts, Prompts},
+    protocol::{
+        JSON_RPC_VERSION,
+        initialize::{
+            InitializeRequest, InitializeResponse, PromptsCapability, ResourcesCapability,
+            ServerCapabilities, ServerInfo, ToolsCapability,
+        },
+        prompts::{PromptsGetRequest, PromptsListResponse},
+        resources::{
+            Resource, ResourceContent, ResourcesListRequest, ResourcesReadRequest,
+            ResourcesReadResponse, ResourcesTemplatesListRequest,
+        },
+        rpc::{Request, RequestId, Requests, Response},
+        tool::{ToolsCallRequest, ToolsListResponse},
+    },
+    resources::{NoResources, Resources},
+    tool::{NoTools, Tools, normalize_schema_value},
+};
+
+/// A server that can be used to handle MCP requests.
+pub struct McpServer<ToolsType = NoTools, PromptsType = NoPrompts, ResourcesType = NoResources> {
+    tools: ToolsType,
+    prompts: PromptsType,
+    resources_handler: ResourcesType,
+    disabled_tools: HashSet<String>,
+    server_info: ServerInfo,
+    resources: Vec<Resource>,
+    resource_contents: HashMap<String, ResourceContent>,
+}
+
+impl Default for McpServer<NoTools, NoPrompts, NoResources> {
+    #[inline]
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl McpServer<NoTools, NoPrompts, NoResources> {
+    /// Creates a new MCP server.
+    #[inline]
+    pub fn new() -> Self {
+        Self {
+            tools: NoTools,
+            prompts: NoPrompts,
+            resources_handler: NoResources,
+            disabled_tools: HashSet::new(),
+            server_info: ServerInfo {
+                name: "poem-mcpserver".to_string(),
+                version: "0.1.0".to_string(),
+            },
+            resources: Vec::new(),
+            resource_contents: HashMap::new(),
+        }
+    }
+}
+
+impl<ToolsType, PromptsType, ResourcesType> McpServer<ToolsType, PromptsType, ResourcesType>
+where
+    ToolsType: Tools,
+    PromptsType: Prompts,
+    ResourcesType: Resources,
+{
+    /// Sets the tools that the server will use.
+    #[inline]
+    pub fn tools<T>(self, tools: T) -> McpServer<T, PromptsType, ResourcesType>
+    where
+        T: Tools,
+    {
+        McpServer {
+            tools,
+            prompts: self.prompts,
+            resources_handler: self.resources_handler,
+            disabled_tools: self.disabled_tools,
+            server_info: self.server_info,
+            resources: self.resources,
+            resource_contents: self.resource_contents,
+        }
+    }
+
+    /// Sets the prompts that the server will use.
+    #[inline]
+    pub fn prompts<P>(self, prompts: P) -> McpServer<ToolsType, P, ResourcesType>
+    where
+        P: Prompts,
+    {
+        McpServer {
+            tools: self.tools,
+            prompts,
+            resources_handler: self.resources_handler,
+            disabled_tools: self.disabled_tools,
+            server_info: self.server_info,
+            resources: self.resources,
+            resource_contents: self.resource_contents,
+        }
+    }
+
+    /// Sets the resources that the server will use.
+    #[inline]
+    pub fn resources<R>(self, resources_handler: R) -> McpServer<ToolsType, PromptsType, R>
+    where
+        R: Resources,
+    {
+        McpServer {
+            tools: self.tools,
+            prompts: self.prompts,
+            resources_handler,
+            disabled_tools: self.disabled_tools,
+            server_info: self.server_info,
+            resources: self.resources,
+            resource_contents: self.resource_contents,
+        }
+    }
+
+    /// Disables tools by their names.
+    pub fn disable_tools<I, T>(mut self, names: I) -> Self
+    where
+        I: IntoIterator<Item = T>,
+        T: Into<String>,
+    {
+        self.disabled_tools
+            .extend(names.into_iter().map(Into::into));
+        self
+    }
+
+    /// Adds a static UI resource.
+    pub fn ui_resource(
+        mut self,
+        uri: impl Into<String>,
+        name: impl Into<String>,
+        description: impl Into<String>,
+        mime_type: impl Into<String>,
+        text: impl Into<String>,
+    ) -> Self {
+        let uri = uri.into();
+        let resource = Resource {
+            uri: uri.clone(),
+            name: name.into(),
+            description: description.into(),
+            mime_type: mime_type.into(),
+        };
+        let content = ResourceContent {
+            uri: uri.clone(),
+            mime_type: resource.mime_type.clone(),
+            text: Some(text.into()),
+            blob: None,
+        };
+        self.resources.push(resource);
+        self.resource_contents.insert(uri, content);
+        self
+    }
+
+    /// Sets the server info (name and version).
+    pub fn with_server_info(mut self, name: &str, version: &str) -> Self {
+        self.server_info = ServerInfo {
+            name: name.to_string(),
+            version: version.to_string(),
+        };
+        self
+    }
+
+    fn handle_ping(&self, id: Option<RequestId>) -> Response<Value> {
+        Response {
+            jsonrpc: JSON_RPC_VERSION.to_string(),
+            id,
+            result: Some(Value::Object(Default::default())),
+            error: None,
+        }
+    }
+
+    fn handle_initialize(
+        &self,
+        request: InitializeRequest,
+        id: Option<RequestId>,
+    ) -> Response<Value> {
+        Response {
+            jsonrpc: JSON_RPC_VERSION.to_string(),
+            id,
+            result: Some(InitializeResponse {
+                protocol_version: request.protocol_version,
+                capabilities: ServerCapabilities {
+                    prompts: PromptsCapability {
+                        list_changed: false,
+                    },
+                    resources: ResourcesCapability {
+                        list_changed: false,
+                        subscribe: false,
+                    },
+                    tools: ToolsCapability {
+                        list_changed: false,
+                    },
+                },
+                server_info: self.server_info.clone(),
+                instructions: Some(ToolsType::instructions().to_string()),
+            }),
+            error: None,
+        }
+        .map_result_to_value()
+    }
+
+    fn handle_tools_list(&self, id: Option<RequestId>) -> Response<Value> {
+        Response {
+            jsonrpc: JSON_RPC_VERSION.to_string(),
+            id,
+            result: Some(ToolsListResponse {
+                tools: {
+                    let mut tools = ToolsType::list();
+                    tools.retain(|tool| !self.disabled_tools.contains(tool.name));
+
+                    for tool in &mut tools {
+                        tool.input_schema =
+                            normalize_schema_value(std::mem::take(&mut tool.input_schema));
+                        tool.output_schema = tool.output_schema.take().map(normalize_schema_value);
+
+                        if let Some(object) = tool.input_schema.as_object_mut() {
+                            if !object.contains_key("properties") {
+                                object.insert(
+                                    "properties".to_string(),
+                                    Value::Object(Default::default()),
+                                );
+                            }
+                        }
+                    }
+                    tools
+                },
+            }),
+            error: None,
+        }
+        .map_result_to_value()
+    }
+
+    async fn handle_tools_call(
+        &mut self,
+        request: ToolsCallRequest,
+        id: Option<RequestId>,
+    ) -> Response<Value> {
+        match self.tools.call(&request.name, request.arguments).await {
+            Ok(response) => Response {
+                jsonrpc: JSON_RPC_VERSION.to_string(),
+                id,
+                result: Some(response),
+                error: None,
+            }
+            .map_result_to_value(),
+            Err(err) => Response::<()> {
+                jsonrpc: JSON_RPC_VERSION.to_string(),
+                id,
+                result: None,
+                error: Some(err),
+            }
+            .map_result_to_value(),
+        }
+    }
+
+    fn handle_prompts_list(&self, id: Option<RequestId>) -> Response<Value> {
+        Response {
+            jsonrpc: JSON_RPC_VERSION.to_string(),
+            id,
+            result: Some(PromptsListResponse {
+                prompts: PromptsType::list(),
+            }),
+            error: None,
+        }
+        .map_result_to_value()
+    }
+
+    async fn handle_prompts_get(
+        &self,
+        request: PromptsGetRequest,
+        id: Option<RequestId>,
+    ) -> Response<Value> {
+        match self.prompts.get(&request.name, request.arguments).await {
+            Ok(response) => Response {
+                jsonrpc: JSON_RPC_VERSION.to_string(),
+                id,
+                result: Some(response),
+                error: None,
+            }
+            .map_result_to_value(),
+            Err(err) => Response::<()> {
+                jsonrpc: JSON_RPC_VERSION.to_string(),
+                id,
+                result: None,
+                error: Some(err),
+            }
+            .map_result_to_value(),
+        }
+    }
+
+    async fn handle_resources_list(
+        &self,
+        request: ResourcesListRequest,
+        id: Option<RequestId>,
+    ) -> Response<Value> {
+        match self.resources_handler.list(request).await {
+            Ok(mut response) => {
+                response.resources.extend(self.resources.clone());
+                Response {
+                    jsonrpc: JSON_RPC_VERSION.to_string(),
+                    id,
+                    result: Some(response),
+                    error: None,
+                }
+                .map_result_to_value()
+            }
+            Err(err) => Response::<()> {
+                jsonrpc: JSON_RPC_VERSION.to_string(),
+                id,
+                result: None,
+                error: Some(err),
+            }
+            .map_result_to_value(),
+        }
+    }
+
+    async fn handle_resources_templates_list(
+        &self,
+        request: ResourcesTemplatesListRequest,
+        id: Option<RequestId>,
+    ) -> Response<Value> {
+        match self.resources_handler.templates(request).await {
+            Ok(response) => Response {
+                jsonrpc: JSON_RPC_VERSION.to_string(),
+                id,
+                result: Some(response),
+                error: None,
+            }
+            .map_result_to_value(),
+            Err(err) => Response::<()> {
+                jsonrpc: JSON_RPC_VERSION.to_string(),
+                id,
+                result: None,
+                error: Some(err),
+            }
+            .map_result_to_value(),
+        }
+    }
+
+    async fn handle_resources_read(
+        &self,
+        request: ResourcesReadRequest,
+        id: Option<RequestId>,
+    ) -> Response<Value> {
+        match self.resource_contents.get(&request.uri) {
+            Some(content) => Response {
+                jsonrpc: JSON_RPC_VERSION.to_string(),
+                id,
+                result: Some(ResourcesReadResponse {
+                    contents: vec![content.clone()],
+                }),
+                error: None,
+            }
+            .map_result_to_value(),
+            None => match self.resources_handler.read(request).await {
+                Ok(response) => Response {
+                    jsonrpc: JSON_RPC_VERSION.to_string(),
+                    id,
+                    result: Some(response),
+                    error: None,
+                }
+                .map_result_to_value(),
+                Err(err) => Response::<()> {
+                    jsonrpc: JSON_RPC_VERSION.to_string(),
+                    id,
+                    result: None,
+                    error: Some(err),
+                }
+                .map_result_to_value(),
+            },
+        }
+    }
+
+    /// Handles a request and returns a response.
+    pub async fn handle_request(&mut self, request: Request) -> Option<Response<Value>> {
+        match request.body {
+            Requests::Ping => Some(self.handle_ping(request.id)),
+            Requests::Initialize { params } => Some(self.handle_initialize(params, request.id)),
+            Requests::Initialized => None,
+            Requests::Cancelled { .. } => None,
+            Requests::ToolsList { .. } => Some(self.handle_tools_list(request.id)),
+            Requests::ToolsCall { params } => {
+                Some(self.handle_tools_call(params, request.id).await)
+            }
+            Requests::PromptsList { .. } => Some(self.handle_prompts_list(request.id)),
+            Requests::PromptsGet { params } => {
+                Some(self.handle_prompts_get(params, request.id).await)
+            }
+            Requests::ResourcesList { params } => {
+                Some(self.handle_resources_list(params, request.id).await)
+            }
+            Requests::ResourcesTemplatesList { params } => Some(
+                self.handle_resources_templates_list(params, request.id)
+                    .await,
+            ),
+            Requests::ResourcesRead { params } => {
+                Some(self.handle_resources_read(params, request.id).await)
+            }
+        }
+    }
+}
