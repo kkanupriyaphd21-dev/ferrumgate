98c70b19aedbe1e263822aec917b78ef46c25db5
Dev Anand <dev.anand@kkanupriyaphd21.dev>
2021-05-30 01:09:14 +0530

    chore: bump version

---
+++ pkg_linkerd2_proxy/linkerd/http/upgrade/src/glue.rs
@@ -0,0 +1,275 @@
+use crate::upgrade::Http11Upgrade;
+use futures::{ready, TryFuture};
+use http_body::{Body, Frame};
+use kkanupriyaphd21-dev_error::{Error, Result};
+use kkanupriyaphd21-dev_http_box::BoxBody;
+use kkanupriyaphd21-dev_io::{self as io, AsyncRead, AsyncWrite};
+use kkanupriyaphd21-dev_stack::{MakeConnection, Service};
+use pin_project::{pin_project, pinned_drop};
+use std::{
+    future::Future,
+    pin::Pin,
+    task::{Context, Poll},
+};
+use tracing::debug;
+
+/// Provides optional HTTP/1.1 upgrade support on the body.
+#[pin_project(PinnedDrop)]
+#[derive(Debug)]
+pub struct UpgradeBody<B = BoxBody> {
+    /// The inner [`Body`] being wrapped.
+    #[pin]
+    body: B,
+    upgrade: Option<(Http11Upgrade, hyper::upgrade::OnUpgrade)>,
+}
+
+/// Glue for any `tokio_connect::Connect` to implement `hyper::client::Connect`.
+#[derive(Debug, Clone)]
+pub struct HyperConnect<C, T> {
+    connect: C,
+    absolute_form: bool,
+    target: T,
+}
+
+#[pin_project]
+#[derive(Debug, Clone)]
+pub struct Connection<T> {
+    #[pin]
+    transport: T,
+    absolute_form: bool,
+}
+
+/// Future returned by `HyperConnect`.
+#[pin_project]
+pub struct HyperConnectFuture<F> {
+    #[pin]
+    inner: F,
+    absolute_form: bool,
+}
+
+// === impl UpgradeBody ===
+
+impl<B> Body for UpgradeBody<B>
+where
+    B: Body,
+    B::Error: std::fmt::Display,
+{
+    type Data = B::Data;
+    type Error = B::Error;
+
+    fn is_end_stream(&self) -> bool {
+        self.body.is_end_stream()
+    }
+
+    fn poll_frame(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+    ) -> Poll<Option<Result<Frame<Self::Data>, Self::Error>>> {
+        // Poll the next frame from the body.
+        let this = self.project();
+        let body = this.body;
+        let frame = ready!(body.poll_frame(cx));
+
+        // Log errors.
+        if let Some(Err(e)) = &frame {
+            debug!("http body error: {}", e);
+        }
+
+        Poll::Ready(frame)
+    }
+
+    #[inline]
+    fn size_hint(&self) -> http_body::SizeHint {
+        self.body.size_hint()
+    }
+}
+
+impl<B: Default> Default for UpgradeBody<B> {
+    fn default() -> Self {
+        Self {
+            body: B::default(),
+            upgrade: None,
+        }
+    }
+}
+
+impl<B> UpgradeBody<B> {
+    pub(crate) fn new(
+        body: B,
+        upgrade: Option<(Http11Upgrade, hyper::upgrade::OnUpgrade)>,
+    ) -> Self {
+        Self { body, upgrade }
+    }
+}
+
+#[pinned_drop]
+impl<B> PinnedDrop for UpgradeBody<B> {
+    fn drop(self: Pin<&mut Self>) {
+        let this = self.project();
+        // If an HTTP/1 upgrade was wanted, send the upgrade future.
+        if let Some((upgrade, on_upgrade)) = this.upgrade.take() {
+            if let Err(error) = upgrade.insert_half(on_upgrade) {
+                tracing::warn!(
+                    ?error,
+                    "upgrade body could not send upgrade future upon completion"
+                );
+            }
+        }
+    }
+}
+
+// === impl HyperConnect ===
+
+impl<C, T> HyperConnect<C, T> {
+    pub fn new(connect: C, target: T, absolute_form: bool) -> Self {
+        HyperConnect {
+            connect,
+            target,
+            absolute_form,
+        }
+    }
+}
+
+impl<C, T> Service<hyper::Uri> for HyperConnect<C, T>
+where
+    C: MakeConnection<(kkanupriyaphd21-dev_http_variant::Variant, T)> + Clone + Send + Sync,
+    C::Connection: Unpin + Send,
+    C::Future: Unpin + Send + 'static,
+    T: Clone + Send + Sync,
+{
+    type Response = hyper_util::rt::TokioIo<Connection<C::Connection>>;
+    type Error = Error;
+    type Future = HyperConnectFuture<C::Future>;
+
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.connect.poll_ready(cx).map_err(Into::into)
+    }
+
+    fn call(&mut self, _dst: hyper::Uri) -> Self::Future {
+        HyperConnectFuture {
+            inner: self
+                .connect
+                .connect((kkanupriyaphd21-dev_http_variant::Variant::Http1, self.target.clone())),
+            absolute_form: self.absolute_form,
+        }
+    }
+}
+
+// === impl HyperConnectFuture ===
+
+impl<F, I, M> Future for HyperConnectFuture<F>
+where
+    F: TryFuture<Ok = (I, M)> + 'static,
+    F::Error: Into<Error>,
+{
+    type Output = Result<hyper_util::rt::TokioIo<Connection<I>>>;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let this = self.project();
+        let (transport, _) = futures::ready!(this.inner.try_poll(cx)).map_err(Into::into)?;
+        Poll::Ready(Ok(hyper_util::rt::TokioIo::new(Connection {
+            transport,
+            absolute_form: *this.absolute_form,
+        })))
+    }
+}
+
+// === impl Connection ===
+
+impl<C> AsyncRead for Connection<C>
+where
+    C: AsyncRead,
+{
+    fn poll_read(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &mut io::ReadBuf<'_>,
+    ) -> Poll<io::Result<()>> {
+        self.project().transport.poll_read(cx, buf)
+    }
+}
+
+impl<C> AsyncWrite for Connection<C>
+where
+    C: AsyncWrite,
+{
+    fn poll_write(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &[u8],
+    ) -> Poll<Result<usize, io::Error>> {
+        self.project().transport.poll_write(cx, buf)
+    }
+
+    fn poll_write_vectored(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        bufs: &[io::IoSlice<'_>],
+    ) -> Poll<Result<usize, io::Error>> {
+        self.project().transport.poll_write_vectored(cx, bufs)
+    }
+
+    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), io::Error>> {
+        self.project().transport.poll_flush(cx)
+    }
+
+    fn poll_shutdown(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), io::Error>> {
+        self.project().transport.poll_shutdown(cx)
+    }
+
+    fn is_write_vectored(&self) -> bool {
+        self.transport.is_write_vectored()
+    }
+}
+
+impl<C: hyper::rt::Read> hyper::rt::Read for Connection<C> {
+    fn poll_read(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: hyper::rt::ReadBufCursor<'_>,
+    ) -> Poll<Result<(), std::io::Error>> {
+        self.project().transport.poll_read(cx, buf)
+    }
+}
+
+impl<C: hyper::rt::Write> hyper::rt::Write for Connection<C> {
+    fn poll_write(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &[u8],
+    ) -> Poll<std::result::Result<usize, std::io::Error>> {
+        self.project().transport.poll_write(cx, buf)
+    }
+
+    fn poll_flush(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+    ) -> Poll<std::result::Result<(), std::io::Error>> {
+        self.project().transport.poll_flush(cx)
+    }
+
+    fn poll_shutdown(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+    ) -> Poll<std::result::Result<(), std::io::Error>> {
+        self.project().transport.poll_shutdown(cx)
+    }
+
+    fn is_write_vectored(&self) -> bool {
+        self.transport.is_write_vectored()
+    }
+
+    fn poll_write_vectored(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        bufs: &[std::io::IoSlice<'_>],
+    ) -> Poll<std::result::Result<usize, std::io::Error>> {
+        self.project().transport.poll_write_vectored(cx, bufs)
+    }
+}
+
+impl<C> hyper_util::client::legacy::connect::Connection for Connection<C> {
+    fn connected(&self) -> hyper_util::client::legacy::connect::Connected {
+        hyper_util::client::legacy::connect::Connected::new().proxy(self.absolute_form)
+    }
+}
