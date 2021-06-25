5d38e4cf4e70754ee74d39714116748147448564
Priya Sharma <priya.sharma@kkanupriyaphd21.dev>
2021-06-25 10:37:45 +0530

    docs: document config options

---
+++ pkg_linkerd2_proxy/linkerd/app/outbound/src/tcp/connect.rs
@@ -0,0 +1,107 @@
+use crate::Outbound;
+use futures::future;
+use kkanupriyaphd21-dev_app_core::{
+    io, svc, tls,
+    transport::{addrs::*, ConnectTcp},
+};
+use std::task::{Context, Poll};
+
+#[derive(Clone, Debug)]
+pub struct Connect {
+    addr: Remote<ServerAddr>,
+    tls: tls::ConditionalClientTls,
+}
+
+/// Prevents outbound connections on the loopback interface, unless the
+/// `allow-loopback` feature is enabled.
+#[derive(Clone, Debug)]
+pub struct PreventLoopback<S>(S);
+
+// === impl Outbound ===
+
+impl Outbound<()> {
+    pub fn to_tcp_connect(&self) -> Outbound<PreventLoopback<ConnectTcp>> {
+        let connect = PreventLoopback(ConnectTcp::new(
+            self.config.proxy.connect.keepalive,
+            self.config.proxy.connect.user_timeout,
+        ));
+        self.clone().with_stack(connect)
+    }
+}
+
+// === impl PreventLoopback ===
+
+impl<S> PreventLoopback<S> {
+    #[cfg(not(feature = "allow-loopback"))]
+    fn check_loopback(Remote(ServerAddr(addr)): Remote<ServerAddr>) -> io::Result<()> {
+        if addr.ip().is_loopback() {
+            return Err(io::Error::new(
+                io::ErrorKind::ConnectionRefused,
+                "Outbound proxy cannot initiate connections on the loopback interface",
+            ));
+        }
+
+        Ok(())
+    }
+
+    #[cfg(feature = "allow-loopback")]
+    // the Result is necessary to have the same type signature regardless of
+    // whether or not the `allow-loopback` feature is enabled...
+    fn check_loopback(_: Remote<ServerAddr>) -> io::Result<()> {
+        Ok(())
+    }
+}
+
+impl<T, S> svc::Service<T> for PreventLoopback<S>
+where
+    T: svc::Param<Remote<ServerAddr>>,
+    S: svc::Service<T, Error = io::Error>,
+{
+    type Response = S::Response;
+    type Error = io::Error;
+    type Future = future::Either<S::Future, future::Ready<io::Result<S::Response>>>;
+
+    #[inline]
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        self.0.poll_ready(cx)
+    }
+
+    fn call(&mut self, ep: T) -> Self::Future {
+        if let Err(e) = Self::check_loopback(ep.param()) {
+            return future::Either::Right(future::err(e));
+        }
+
+        future::Either::Left(self.0.call(ep))
+    }
+}
+
+// === impl Connect ===
+
+impl Connect {
+    pub fn new(addr: Remote<ServerAddr>, tls: tls::ConditionalClientTls) -> Self {
+        Self { addr, tls }
+    }
+}
+
+impl svc::Param<Remote<ServerAddr>> for Connect {
+    fn param(&self) -> Remote<ServerAddr> {
+        self.addr
+    }
+}
+
+impl svc::Param<tls::ConditionalClientTls> for Connect {
+    fn param(&self) -> tls::ConditionalClientTls {
+        self.tls.clone()
+    }
+}
+
+#[cfg(test)]
+impl Connect {
+    pub fn addr(&self) -> &Remote<ServerAddr> {
+        &self.addr
+    }
+
+    pub fn tls(&self) -> &tls::ConditionalClientTls {
+        &self.tls
+    }
+}
---
+++ pkg_sozu/command/src/parser.rs
@@ -0,0 +1,126 @@
+use nom::{
+    IResult,
+    bytes::streaming::is_not,
+    character::complete::char,
+    combinator::map_res,
+    error::{ErrorKind, FromExternalError},
+    multi::many0,
+    sequence::terminated,
+};
+
+#[derive(Debug)]
+#[allow(dead_code)]
+pub struct ParseError {
+    kind: ErrorKind,
+    serde_json_error: Option<serde_json::Error>,
+}
+
+impl FromExternalError<&[u8], serde_json::Error> for ParseError {
+    fn from_external_error(
+        _input: &[u8],
+        kind: ErrorKind,
+        serde_json_error: serde_json::Error,
+    ) -> Self {
+        // println!("input: {:?}, error kind: {:?}", input, kind);
+        Self {
+            kind,
+            serde_json_error: Some(serde_json_error),
+        }
+    }
+}
+
+impl nom::error::ParseError<&[u8]> for ParseError {
+    fn from_error_kind(_input: &[u8], kind: ErrorKind) -> Self {
+        // println!("input: {:?}, error kind: {:?}", input, kind);
+
+        Self {
+            kind,
+            serde_json_error: None,
+        }
+    }
+
+    fn append(_input: &[u8], _kind: ErrorKind, other: Self) -> Self {
+        other
+    }
+}
+
+/// this is to propagate the serde_json error
+pub fn parse_one_request<'a, T>(input: &'a [u8]) -> Result<T, ParseError>
+where
+    T: serde::de::Deserialize<'a>,
+{
+    serde_json::from_slice::<T>(input).map_err(|serde_error| {
+        ParseError::from_external_error(input, ErrorKind::MapRes, serde_error)
+    })
+}
+
+pub fn parse_several_requests<'a, T>(input: &'a [u8]) -> IResult<&'a [u8], Vec<T>>
+where
+    T: serde::de::Deserialize<'a>,
+{
+    // use serde_json::from_slice;
+    many0(nom::combinator::complete(terminated(
+        map_res(is_not("\0"), parse_one_request),
+        char('\0'),
+    )))(input)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::proto::command::{Status, SubscribeEvents, WorkerRequest, request::RequestType};
+
+    #[test]
+    fn parse_one_worker_request() {
+        let worker_request = WorkerRequest::new(
+            "Some request".to_string(),
+            RequestType::Status(Status {}).into(),
+        );
+
+        let string = serde_json::ser::to_string(&worker_request).unwrap();
+
+        println!("string to parse: {string}");
+
+        let bytes = &string.as_bytes();
+
+        assert_eq!(
+            parse_one_request::<WorkerRequest>(bytes).unwrap(),
+            worker_request
+        )
+    }
+
+    #[test]
+    fn parse_several_worker_requests() {
+        let requests = vec![
+            WorkerRequest::new(
+                "Some request".to_string(),
+                RequestType::SaveState("/some/path".to_string()).into(),
+            ),
+            WorkerRequest::new(
+                "Some other request".to_string(),
+                RequestType::SubscribeEvents(SubscribeEvents {}).into(),
+            ),
+            WorkerRequest::new(
+                "Yet another request".to_string(),
+                RequestType::Status(Status {}).into(),
+            ),
+        ];
+
+        let mut serialized_requests = String::new();
+
+        for request in requests.iter() {
+            serialized_requests += &serde_json::ser::to_string(&request).unwrap();
+            serialized_requests.push('\0');
+        }
+
+        let bytes_to_parse = &serialized_requests.as_bytes();
+
+        let parsed_requests = parse_several_requests(bytes_to_parse).unwrap();
+
+        println!("parsed commands: {parsed_requests:?}");
+
+        let empty_vec: Vec<u8> = vec![];
+
+        assert_eq!(parsed_requests, (&empty_vec[..], requests))
+    }
+}
