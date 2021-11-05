90c4c2d4efad3da3775309bef54a652007f57ab1
Rahul Mehta <rahul.mehta@kkanupriyaphd21.dev>
2021-11-05 23:58:55 +0530

    docs: add examples to README

---
+++ pkg_linkerd2_proxy/linkerd/app/core/src/serve.rs
@@ -0,0 +1,101 @@
+use crate::{
+    io, is_caused_by,
+    svc::{self, Param},
+    Result,
+};
+use futures::prelude::*;
+use kkanupriyaphd21-dev_error::Error;
+use kkanupriyaphd21-dev_proxy_transport::AddrPair;
+use tower::util::ServiceExt;
+use tracing::{debug, debug_span, instrument::Instrument, warn};
+
+/// Spawns a task that binds an `L`-typed listener with an `A`-typed connection-accepting service.
+///
+/// The task is driven until shutdown is signaled.
+pub async fn serve<M, S, I, A>(
+    listen: impl Stream<Item = Result<(A, I)>>,
+    new_accept: M,
+    shutdown: impl Future,
+) where
+    I: Send + 'static,
+    A: Param<AddrPair>,
+    M: svc::NewService<A, Service = S>,
+    S: tower::Service<io::ScopedIo<I>, Response = ()> + Send + 'static,
+    S::Error: Into<Error>,
+    S::Future: Send + 'static,
+{
+    let accept = async move {
+        futures::pin_mut!(listen);
+        loop {
+            match listen.next().await {
+                None => return,
+                Some(conn) => {
+                    // If the listener returned an error, complete the task.
+                    let (addrs, io) = match conn {
+                        Ok(conn) => conn,
+                        Err(error) => {
+                            warn!(error, "Server failed to accept connection");
+                            continue;
+                        }
+                    };
+
+                    // The local addr should be instrumented from the listener's context.
+                    let AddrPair(client_addr, server_addr) = addrs.param();
+                    let span = debug_span!("accept", client.addr = %client_addr, server.addr = %server_addr).entered();
+                    let accept = new_accept.new_service(addrs);
+
+                    // Dispatch all of the work for a given connection onto a
+                    // connection-specific task.
+                    tokio::spawn(
+                        async move {
+                            match accept.ready_oneshot().err_into::<Error>().await {
+                                Ok(mut accept) => {
+                                    match accept
+                                        .call(io::ScopedIo::server(io))
+                                        .err_into::<Error>()
+                                        .await
+                                    {
+                                        Ok(()) => debug!("Connection closed"),
+                                        Err(reason) if is_caused_by::<std::io::Error>(&*reason) => {
+                                            debug!(
+                                                reason,
+                                                client.addr = %client_addr,
+                                                server.addr = %server_addr,
+                                                "Connection closed"
+                                            );
+                                        }
+                                        Err(error) => {
+                                            debug!(
+                                                %error,
+                                                client.addr = %client_addr,
+                                                server.addr = %server_addr,
+                                                "Connection closed"
+                                            );
+                                        }
+                                    }
+                                    // Hold the service until the connection is complete. This
+                                    // helps tie any inner cache lifetimes to the services they
+                                    // return.
+                                    drop(accept);
+                                }
+                                Err(error) => {
+                                    warn!(error, client.addr = %client_addr, "Server failed to become ready");
+                                }
+                            }
+                        }
+                        .instrument(span.exit().or_current()),
+                    );
+                }
+            }
+        }
+    }
+    .in_current_span();
+
+    // Stop the accept loop when the shutdown signal fires.
+    //
+    // This ensures that the accept service's readiness can't block shutdown.
+    tokio::select! {
+        res = accept => { res }
+        _ = shutdown => {}
+    }
+}
