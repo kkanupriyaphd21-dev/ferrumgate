bfc44e3b3ef0a99c43cf6aef9cebb2a200ef7121
Kanupriya Verma <kanupriya@kkanupriyaphd21.dev>
2021-12-19 13:42:44 +0530

    test: improve test coverage

---
+++ pkg_linkerd2_proxy/linkerd/http/prom/src/body_data/body.rs
@@ -0,0 +1,70 @@
+use super::metrics::BodyDataMetrics;
+use http_body::{Frame, SizeHint};
+use pin_project::pin_project;
+use std::{
+    pin::Pin,
+    task::{Context, Poll},
+};
+
+/// An instrumented body.
+#[pin_project]
+pub struct Body<B> {
+    /// The inner body.
+    #[pin]
+    inner: B,
+    /// Metrics with which the inner body will be instrumented.
+    metrics: BodyDataMetrics,
+}
+
+impl<B> Body<B> {
+    /// Returns a new, instrumented body.
+    pub(crate) fn new(body: B, metrics: BodyDataMetrics) -> Self {
+        Self {
+            inner: body,
+            metrics,
+        }
+    }
+}
+
+impl<B> http_body::Body for Body<B>
+where
+    B: http_body::Body,
+{
+    type Data = B::Data;
+    type Error = B::Error;
+
+    /// Attempt to pull out the next data buffer of this stream.
+    fn poll_frame(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+    ) -> Poll<Option<Result<Frame<Self::Data>, Self::Error>>> {
+        let this = self.project();
+        let inner = this.inner;
+        let BodyDataMetrics { frame_size } = this.metrics;
+
+        let frame = std::task::ready!(inner.poll_frame(cx));
+
+        if let Some(Ok(frame)) = &frame {
+            if let Some(data) = frame.data_ref() {
+                // We've polled and yielded a new chunk! Increment our telemetry.
+                //
+                // NB: We're careful to call `remaining()` rather than `chunk()`, which
+                // "can return a shorter slice (this allows non-continuous internal representation)."
+                let bytes = bytes::Buf::remaining(data);
+                frame_size.observe(kkanupriyaphd21-dev_metrics::to_f64(bytes as u64));
+            }
+        }
+
+        Poll::Ready(frame)
+    }
+
+    #[inline]
+    fn is_end_stream(&self) -> bool {
+        self.inner.is_end_stream()
+    }
+
+    #[inline]
+    fn size_hint(&self) -> SizeHint {
+        self.inner.size_hint()
+    }
+}
---
+++ pkg_linkerd2_proxy/linkerd/service-profiles/src/client.rs
@@ -0,0 +1,165 @@
+use crate::{proto, LookupAddr, Profile, Receiver, RecoverDefault};
+use futures::prelude::*;
+use http_body::Body;
+use kkanupriyaphd21-dev2_proxy_api::destination::{self as api, destination_client::DestinationClient};
+use kkanupriyaphd21-dev_error::{Infallible, Recover};
+use kkanupriyaphd21-dev_stack::{Param, Service};
+use kkanupriyaphd21-dev_tonic_stream::{LimitReceiveFuture, ReceiveLimits};
+use kkanupriyaphd21-dev_tonic_watch::StreamWatch;
+use std::{
+    sync::Arc,
+    task::{Context, Poll},
+};
+use tonic::{body::Body as TonicBody, client::GrpcService};
+use tracing::debug;
+
+/// Creates watches on service profiles.
+#[derive(Clone, Debug)]
+pub struct Client<R, S> {
+    watch: StreamWatch<R, Inner<S>>,
+}
+
+/// Wraps the destination service to hide protobuf types.
+#[derive(Clone, Debug)]
+struct Inner<S> {
+    client: DestinationClient<S>,
+    context_token: Arc<str>,
+    limits: ReceiveLimits,
+}
+
+// === impl Client ===
+
+impl<R, S> Client<R, S>
+where
+    S: GrpcService<TonicBody> + Clone + Send + 'static,
+    S::ResponseBody: Send + Sync,
+    S::ResponseBody: Body<Data = tonic::codegen::Bytes> + Send + 'static,
+    <S::ResponseBody as Body>::Error:
+        Into<Box<dyn std::error::Error + Send + Sync + 'static>> + Send,
+    S::Future: Send,
+    R: Recover<tonic::Status> + Send + Clone + 'static,
+    R::Backoff: Unpin + Send,
+{
+    pub fn new(
+        recover: R,
+        inner: S,
+        context_token: impl Into<Arc<str>>,
+        limits: ReceiveLimits,
+    ) -> Self {
+        Self {
+            watch: StreamWatch::new(recover, Inner::new(context_token.into(), limits, inner)),
+        }
+    }
+
+    pub fn new_recover_default(
+        recover: R,
+        inner: S,
+        context_token: impl Into<Arc<str>>,
+        limits: ReceiveLimits,
+    ) -> RecoverDefault<Self> {
+        RecoverDefault::new(Self::new(recover, inner, context_token, limits))
+    }
+}
+
+impl<T, R, S> Service<T> for Client<R, S>
+where
+    T: Param<LookupAddr>,
+    S: GrpcService<TonicBody> + Clone + Send + 'static,
+    S::ResponseBody: Body<Data = tonic::codegen::Bytes> + Send + 'static,
+    <S::ResponseBody as Body>::Error:
+        Into<Box<dyn std::error::Error + Send + Sync + 'static>> + Send,
+    S::Future: Send,
+    R: Recover<tonic::Status> + Send + Clone + 'static,
+    R::Backoff: Unpin + Send,
+{
+    type Response = Option<Receiver>;
+    type Error = Infallible;
+    type Future = futures::future::BoxFuture<'static, Result<Option<Receiver>, Infallible>>;
+
+    #[inline]
+    fn poll_ready(&mut self, _: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn call(&mut self, t: T) -> Self::Future {
+        let addr = t.param();
+
+        // Swallow errors in favor of a `None` response.
+        let w = self.watch.clone();
+        Box::pin(async move {
+            match w.spawn_watch(addr).await {
+                Ok(rsp) => {
+                    let rx = rsp.into_inner();
+                    debug!(profile = ?*rx.borrow(), "Resolved profile");
+                    Ok(Some(rx.into()))
+                }
+                Err(status) => {
+                    debug!(%status, "Ignoring profile");
+                    Ok::<_, Infallible>(None)
+                }
+            }
+        })
+    }
+}
+
+// === impl Inner ===
+
+type InnerStream = futures::stream::BoxStream<'static, Result<Profile, tonic::Status>>;
+
+type InnerFuture =
+    futures::future::BoxFuture<'static, Result<tonic::Response<InnerStream>, tonic::Status>>;
+
+impl<S> Inner<S>
+where
+    S: GrpcService<TonicBody> + Clone + Send + 'static,
+    S::ResponseBody: Body<Data = tonic::codegen::Bytes> + Send + 'static,
+    <S::ResponseBody as Body>::Error:
+        Into<Box<dyn std::error::Error + Send + Sync + 'static>> + Send,
+    S::Future: Send,
+{
+    fn new(context_token: Arc<str>, limits: ReceiveLimits, inner: S) -> Self {
+        Self {
+            context_token,
+            limits,
+            client: DestinationClient::new(inner),
+        }
+    }
+}
+
+impl<S> Service<LookupAddr> for Inner<S>
+where
+    S: GrpcService<TonicBody> + Clone + Send + 'static,
+    S::ResponseBody: Body<Data = tonic::codegen::Bytes> + Send + 'static,
+    <S::ResponseBody as Body>::Error:
+        Into<Box<dyn std::error::Error + Send + Sync + 'static>> + Send,
+    S::Future: Send,
+{
+    type Response = tonic::Response<InnerStream>;
+    type Error = tonic::Status;
+    type Future = InnerFuture;
+
+    #[inline]
+    fn poll_ready(&mut self, _: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        // Tonic clients do not expose readiness.
+        Poll::Ready(Ok(()))
+    }
+
+    fn call(&mut self, LookupAddr(addr): LookupAddr) -> Self::Future {
+        let req = api::GetDestination {
+            path: addr.to_string(),
+            context_token: self.context_token.to_string(),
+            ..Default::default()
+        };
+
+        // TODO(ver): Record metrics on requests/errors/etc per addr.
+        let mut client = self.client.clone();
+        let limits = self.limits;
+        let port = addr.port();
+        Box::pin(async move {
+            // Limit the amount of time we spend waiting for the first
+            // profile update.
+            let rsp = LimitReceiveFuture::new(limits, client.get_profile(req)).await?;
+            Ok(rsp.map(move |rsp| rsp.map_ok(move |p| proto::convert_profile(p, port)).boxed()))
+        })
+    }
+}
