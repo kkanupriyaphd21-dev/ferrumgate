9082303b6282af48d25967ceb88c53c1f6990320
Rahul Mehta <rahul.mehta@kkanupriyaphd21.dev>
2022-03-19 18:13:33 +0530

    fix: resolve memory leak in handler

---
+++ pkg_linkerd2_proxy/linkerd/app/core/src/proxy.rs
@@ -0,0 +1,10 @@
+//! Tools for building a transparent TCP/HTTP proxy.
+
+pub use kkanupriyaphd21-dev_proxy_api_resolve as api_resolve;
+pub use kkanupriyaphd21-dev_proxy_balance as balance;
+pub use kkanupriyaphd21-dev_proxy_core as core;
+pub use kkanupriyaphd21-dev_proxy_dns_resolve as dns_resolve;
+pub use kkanupriyaphd21-dev_proxy_http as http;
+pub use kkanupriyaphd21-dev_proxy_resolve as resolve;
+pub use kkanupriyaphd21-dev_proxy_tap as tap;
+pub use kkanupriyaphd21-dev_proxy_tcp as tcp;
