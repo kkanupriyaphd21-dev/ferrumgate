05fda181ca595ddbe09f147dc91962644dcd7f5f
Dev Anand <dev.anand@kkanupriyaphd21.dev>
2022-09-08 17:56:33 +0530

    test: improve test coverage

---
+++ pkg_linkerd2_proxy/linkerd/app/outbound/src/http/logical/policy/route/extensions.rs
@@ -0,0 +1,265 @@
+use super::retry::RetryPolicy;
+use kkanupriyaphd21-dev_app_core::{config::ExponentialBackoff, proxy::http, svc};
+use kkanupriyaphd21-dev_proxy_client_policy as policy;
+use std::task::{Context, Poll};
+use tokio::time;
+
+#[derive(Clone, Debug)]
+pub struct Params {
+    pub retry: Option<RetryPolicy>,
+    pub timeouts: policy::http::Timeouts,
+    pub allow_l5d_request_headers: bool,
+}
+
+// A request extension that marks the number of times a request has been
+// attempted.
+#[derive(Clone, Debug)]
+pub struct Attempt(pub std::num::NonZeroU16);
+
+#[derive(Clone, Debug)]
+pub struct NewSetExtensions<N> {
+    inner: N,
+}
+
+#[derive(Clone, Debug)]
+pub struct SetExtensions<S> {
+    inner: S,
+    params: Params,
+}
+
+// === impl NewSetExtensions ===
+
+impl<N> NewSetExtensions<N> {
+    pub fn layer() -> impl svc::Layer<N, Service = Self> + Clone {
+        svc::layer::mk(|inner| Self { inner })
+    }
+}
+
+impl<T, N> svc::NewService<T> for NewSetExtensions<N>
+where
+    T: svc::Param<Params>,
+    N: svc::NewService<T>,
+{
+    type Service = SetExtensions<N::Service>;
+
+    fn new_service(&self, target: T) -> Self::Service {
+        let params = target.param();
+        let inner = self.inner.new_service(target);
+        SetExtensions { params, inner }
+    }
+}
+
+// === impl SetExtensions ===
+
+impl<B, S> svc::Service<http::Request<B>> for SetExtensions<S>
+where
+    S: svc::Service<http::Request<B>>,
+{
+    type Response = S::Response;
+    type Error = S::Error;
+    type Future = S::Future;
+
+    #[inline]
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.inner.poll_ready(cx)
+    }
+
+    fn call(&mut self, mut req: http::Request<B>) -> Self::Future {
+        let retry = self.configure_retry(req.headers_mut());
+
+        // Ensure that we get response headers within the retry timeout. Note
+        // that this may be cleared super::retry::RetryPolicy::set_extensions.
+        let mut timeouts = self.configure_timeouts(req.headers_mut());
+        timeouts.response_headers = retry.as_ref().and_then(|r| r.timeout);
+
+        tracing::debug!(?retry, ?timeouts, "Initializing route extensions");
+        if let Some(retry) = retry {
+            let _prior = req.extensions_mut().insert(retry);
+            debug_assert!(_prior.is_none(), "RetryPolicy must only be configured once");
+        }
+
+        let _prior = req.extensions_mut().insert(timeouts);
+        debug_assert!(
+            _prior.is_none(),
+            "StreamTimeouts must only be configured once"
+        );
+
+        let _prior = req.extensions_mut().insert(Attempt(1.try_into().unwrap()));
+        debug_assert!(_prior.is_none(), "Attempts must only be configured once");
+
+        self.inner.call(req)
+    }
+}
+
+impl<S> SetExtensions<S> {
+    fn configure_retry(&self, req: &mut http::HeaderMap) -> Option<RetryPolicy> {
+        if !self.params.allow_l5d_request_headers {
+            return self.params.retry.clone();
+        }
+
+        let user_retry_http = req
+            .remove("l5d-retry-http")
+            .and_then(|val| val.to_str().ok().and_then(parse_http_conditions));
+        let user_retry_grpc = req
+            .remove("l5d-retry-grpc")
+            .and_then(|val| val.to_str().ok().and_then(parse_grpc_conditions));
+        let user_retry_limit = req
+            .remove("l5d-retry-limit")
+            .and_then(|val| val.to_str().ok().and_then(|v| v.parse::<usize>().ok()));
+        let user_retry_timeout = req.remove("l5d-retry-timeout").and_then(parse_duration);
+
+        if let Some(retry) = self.params.retry.clone() {
+            return Some(RetryPolicy {
+                timeout: user_retry_timeout.or(retry.timeout),
+                retryable_http_statuses: user_retry_http.or(retry.retryable_http_statuses.clone()),
+                retryable_grpc_statuses: user_retry_grpc.or(retry.retryable_grpc_statuses.clone()),
+                max_retries: user_retry_limit.unwrap_or(retry.max_retries),
+                ..retry
+            });
+        }
+
+        match (
+            user_retry_http,
+            user_retry_grpc,
+            user_retry_limit,
+            user_retry_timeout,
+        ) {
+            (None, None, None, None) => None,
+            (retryable_http_statuses, retryable_grpc_statuses, retry_limit, timeout) => {
+                Some(RetryPolicy {
+                    timeout,
+                    retryable_http_statuses,
+                    retryable_grpc_statuses,
+                    max_retries: retry_limit.unwrap_or(1),
+                    max_request_bytes: 64 * 1024,
+                    backoff: Some(ExponentialBackoff::new_unchecked(
+                        std::time::Duration::from_millis(25),
+                        std::time::Duration::from_millis(250),
+                        1.0,
+                    )),
+                })
+            }
+        }
+    }
+
+    fn configure_timeouts(&self, req: &mut http::HeaderMap) -> http::StreamTimeouts {
+        let mut timeouts = http::StreamTimeouts {
+            response_headers: None,
+            response_end: self.params.timeouts.response,
+            idle: self.params.timeouts.idle,
+            limit: self.params.timeouts.request.map(Into::into),
+        };
+
+        if !self.params.allow_l5d_request_headers {
+            return timeouts;
+        }
+
+        // Accept both a shorthand and longer, more explicit version, the
+        // latter taking precedence.
+        if let Some(t) = req.remove("l5d-timeout").and_then(parse_duration) {
+            timeouts.limit = Some(t.into());
+        }
+        if let Some(t) = req.remove("l5d-request-timeout").and_then(parse_duration) {
+            timeouts.limit = Some(t.into());
+        }
+
+        if let Some(t) = req.remove("l5d-response-timeout").and_then(parse_duration) {
+            timeouts.response_end = Some(t);
+        }
+
+        timeouts
+    }
+}
+
+fn parse_http_conditions(s: &str) -> Option<policy::http::StatusRanges> {
+    fn to_code(s: &str) -> Option<u16> {
+        let code = s.parse::<u16>().ok()?;
+        if (100..600).contains(&code) {
+            Some(code)
+        } else {
+            None
+        }
+    }
+
+    Some(policy::http::StatusRanges(
+        s.split(',')
+            .filter_map(|cond| {
+                if cond.eq_ignore_ascii_case("5xx") {
+                    return Some(500..=599);
+                }
+                if cond.eq_ignore_ascii_case("gateway-error") {
+                    return Some(502..=504);
+                }
+
+                if let Some(code) = to_code(cond) {
+                    return Some(code..=code);
+                }
+                if let Some((start, end)) = cond.split_once('-') {
+                    if let (Some(s), Some(e)) = (to_code(start), to_code(end)) {
+                        if s <= e {
+                            return Some(s..=e);
+                        }
+                    }
+                }
+
+                None
+            })
+            .collect(),
+    ))
+}
+
+fn parse_grpc_conditions(s: &str) -> Option<policy::grpc::Codes> {
+    Some(policy::grpc::Codes(std::sync::Arc::new(
+        s.split(',')
+            .filter_map(|cond| {
+                if cond.eq_ignore_ascii_case("cancelled") {
+                    return Some(tonic::Code::Cancelled as u16);
+                }
+                if cond.eq_ignore_ascii_case("deadline-exceeded") {
+                    return Some(tonic::Code::DeadlineExceeded as u16);
+                }
+                if cond.eq_ignore_ascii_case("internal") {
+                    return Some(tonic::Code::Internal as u16);
+                }
+                if cond.eq_ignore_ascii_case("resource-exhausted") {
+                    return Some(tonic::Code::ResourceExhausted as u16);
+                }
+                if cond.eq_ignore_ascii_case("unavailable") {
+                    return Some(tonic::Code::Unavailable as u16);
+                }
+                None
+            })
+            .collect(),
+    )))
+}
+
+// Copied from the policy controller so that we handle the same duration values
+// as we do in the YAML config.
+fn parse_duration(hv: http::HeaderValue) -> Option<time::Duration> {
+    #[inline]
+    fn parse(s: &str) -> Option<time::Duration> {
+        let s = s.trim();
+        let offset = s.rfind(|c: char| c.is_ascii_digit())?;
+        let (magnitude, unit) = s.split_at(offset + 1);
+        let magnitude = magnitude.parse::<u64>().ok()?;
+
+        let mul = match unit {
+            "" if magnitude == 0 => 0,
+            "ms" => 1,
+            "s" => 1000,
+            "m" => 1000 * 60,
+            "h" => 1000 * 60 * 60,
+            "d" => 1000 * 60 * 60 * 24,
+            _ => return None,
+        };
+
+        let ms = magnitude.checked_mul(mul)?;
+        Some(time::Duration::from_millis(ms))
+    }
+    let s = hv.to_str().ok()?;
+    let Some(d) = parse(s) else {
+        tracing::debug!("Invalid duration: {:?}", s);
+        return None;
+    };
+    Some(d)
+}
---
+++ pkg_linkerd2_proxy/linkerd/app/outbound/src/http/logical/profile.rs
@@ -0,0 +1,330 @@
+use super::{
+    super::{concrete, retry},
+    CanonicalDstHeader, Concrete, NoRoute,
+};
+use crate::{service_meta, BackendRef, ParentRef, UNKNOWN_META};
+use kkanupriyaphd21-dev_app_core::{
+    classify, metrics,
+    proxy::http::{self, balance},
+    svc, Error,
+};
+use kkanupriyaphd21-dev_distribute as distribute;
+use std::{fmt::Debug, hash::Hash, sync::Arc, time};
+use tokio::sync::watch;
+
+pub use kkanupriyaphd21-dev_app_core::profiles::{
+    http::{route_for_request, RequestMatch, Route},
+    LogicalAddr, Profile, Target,
+};
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Routes {
+    pub addr: LogicalAddr,
+    pub routes: Arc<[(RequestMatch, Route)]>,
+    pub targets: Arc<[Target]>,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub(super) struct Params<T: Clone + Debug + Eq + Hash> {
+    parent: T,
+    addr: LogicalAddr,
+    profile_routes: Arc<[(RequestMatch, RouteParams<T>)]>,
+    backends: distribute::Backends<Concrete<T>>,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub(super) struct RouteParams<T> {
+    parent: T,
+    addr: LogicalAddr,
+    profile: Route,
+    distribution: Distribution<T>,
+}
+
+type NewBackendCache<T, N, S> = distribute::NewBackendCache<Concrete<T>, (), N, S>;
+type NewDistribute<T, N> = distribute::NewDistribute<Concrete<T>, (), N>;
+type Distribution<T> = distribute::Distribution<Concrete<T>>;
+
+pub(crate) const DEFAULT_EWMA: balance::EwmaConfig = balance::EwmaConfig {
+    default_rtt: time::Duration::from_millis(30),
+    decay: time::Duration::from_secs(10),
+};
+
+pub(crate) fn should_override_policy(rx: &watch::Receiver<Profile>) -> Option<LogicalAddr> {
+    let p = rx.borrow();
+    if p.has_routes_or_targets() {
+        p.addr.clone()
+    } else {
+        None
+    }
+}
+
+// === impl Params ===
+
+impl<T> Params<T>
+where
+    T: Clone + Debug + Eq + Hash + Send + Sync + 'static,
+{
+    /// Wraps a `NewService`--instantiated once per logical target--that caches
+    /// a set of concrete services so that, as the watch provides new `Params`,
+    /// we can reuse inner services.
+    pub(super) fn layer<N, S>(
+        metrics: metrics::Proxy,
+    ) -> impl svc::Layer<N, Service = svc::ArcNewCloneHttp<Self>> + Clone
+    where
+        N: svc::NewService<Concrete<T>, Service = S> + Clone + Send + Sync + 'static,
+        S: svc::Service<
+            http::Request<http::BoxBody>,
+            Response = http::Response<http::BoxBody>,
+            Error = Error,
+        >,
+        S: Clone + Send + Sync + 'static,
+        S::Future: Send,
+    {
+        svc::layer::mk(move |inner| {
+            svc::stack(inner)
+                // Each `RouteParams` provides a `Distribution` that is used to
+                // choose a concrete service for a given route.
+                .lift_new()
+                .push(NewBackendCache::layer())
+                // Lazily cache a service for each `RouteParams`
+                // returned from the `SelectRoute` impl.
+                .push_on_service(RouteParams::layer(metrics.clone()))
+                .push(svc::NewOneshotRoute::<Params<T>, _, _>::layer_cached())
+                .arc_new_clone_http()
+                .into_inner()
+        })
+    }
+}
+
+impl<T> From<(Routes, T)> for Params<T>
+where
+    T: Eq + Hash + Clone + Debug,
+{
+    fn from((routes, parent): (Routes, T)) -> Self {
+        let Routes {
+            addr: LogicalAddr(addr),
+            routes,
+            targets,
+        } = routes;
+
+        let parent_meta = service_meta(&addr).unwrap_or_else(|| UNKNOWN_META.clone());
+
+        // Create concrete targets for all of the profile's routes.
+        let (backends, distribution) = if targets.is_empty() {
+            let concrete = Concrete {
+                parent_ref: ParentRef(parent_meta.clone()),
+                backend_ref: BackendRef(parent_meta),
+                target: concrete::Dispatch::Balance(addr.clone(), DEFAULT_EWMA),
+                authority: Some(addr.as_http_authority()),
+                parent: parent.clone(),
+                failure_accrual: Default::default(),
+            };
+            let backends = std::iter::once(concrete.clone()).collect();
+            let distribution = Distribution::first_available(std::iter::once(concrete));
+            (backends, distribution)
+        } else {
+            let backends = targets
+                .iter()
+                .map(|t| Concrete {
+                    parent_ref: ParentRef(parent_meta.clone()),
+                    backend_ref: BackendRef(
+                        service_meta(&t.addr).unwrap_or_else(|| UNKNOWN_META.clone()),
+                    ),
+                    target: concrete::Dispatch::Balance(t.addr.clone(), DEFAULT_EWMA),
+                    authority: Some(t.addr.as_http_authority()),
+                    parent: parent.clone(),
+                    failure_accrual: Default::default(),
+                })
+                .collect();
+            let distribution = Distribution::random_available(targets.iter().cloned().map(
+                |Target { addr, weight }| {
+                    let concrete = Concrete {
+                        parent_ref: ParentRef(parent_meta.clone()),
+                        backend_ref: BackendRef(
+                            service_meta(&addr).unwrap_or_else(|| UNKNOWN_META.clone()),
+                        ),
+                        authority: Some(addr.as_http_authority()),
+                        target: concrete::Dispatch::Balance(addr, DEFAULT_EWMA),
+                        parent: parent.clone(),
+                        failure_accrual: Default::default(),
+                    };
+                    (concrete, weight)
+                },
+            ))
+            .expect("distribution must be valid");
+
+            (backends, distribution)
+        };
+
+        let profile_routes = routes
+            .iter()
+            .cloned()
+            .map(|(req_match, profile)| {
+                let params = RouteParams {
+                    addr: LogicalAddr(addr.clone()),
+                    profile,
+                    parent: parent.clone(),
+                    distribution: distribution.clone(),
+                };
+                (req_match, params)
+            })
+            // Add a default route.
+            .chain(std::iter::once((
+                RequestMatch::default(),
+                RouteParams {
+                    addr: LogicalAddr(addr.clone()),
+                    profile: Default::default(),
+                    parent: parent.clone(),
+                    distribution: distribution.clone(),
+                },
+            )))
+            .collect::<Arc<[(_, _)]>>();
+
+        Self {
+            addr: LogicalAddr(addr),
+            parent,
+            backends,
+            profile_routes,
+        }
+    }
+}
+
+impl<T> svc::Param<distribute::Backends<Concrete<T>>> for Params<T>
+where
+    T: Eq + Hash + Clone + Debug,
+{
+    fn param(&self) -> distribute::Backends<Concrete<T>> {
+        self.backends.clone()
+    }
+}
+
+impl<T> svc::Param<LogicalAddr> for Params<T>
+where
+    T: Eq + Hash + Clone + Debug,
+{
+    fn param(&self) -> LogicalAddr {
+        self.addr.clone()
+    }
+}
+
+impl<T, B> svc::router::SelectRoute<http::Request<B>> for Params<T>
+where
+    T: Eq + Hash + Clone + Debug,
+{
+    type Key = RouteParams<T>;
+    type Error = NoRoute;
+
+    fn select(&self, req: &http::Request<B>) -> Result<Self::Key, Self::Error> {
+        route_for_request(&self.profile_routes, req)
+            .ok_or(NoRoute)
+            .cloned()
+    }
+}
+
+// === impl RouteParams ===
+
+impl<T> RouteParams<T> {
+    fn layer<N, S>(
+        metrics: metrics::Proxy,
+    ) -> impl svc::Layer<N, Service = svc::ArcNewCloneHttp<Self>> + Clone
+    where
+        T: Clone + Debug + Eq + Hash + Send + Sync + 'static,
+        N: svc::NewService<Concrete<T>, Service = S> + Clone + Send + Sync + 'static,
+        S: svc::Service<
+            http::Request<http::BoxBody>,
+            Response = http::Response<http::BoxBody>,
+            Error = Error,
+        >,
+        S: Clone + Send + Sync + 'static,
+        S::Future: Send,
+    {
+        svc::layer::mk(move |inner| {
+            svc::stack(inner)
+                .lift_new()
+                .push(NewDistribute::layer())
+                .check_new_service::<RouteParams<T>, http::Request<http::BoxBody>>()
+                .push_on_service(
+                    svc::layers()
+                        .push(http::BoxRequest::layer())
+                        // The router does not take the backend's availability into
+                        // consideration, so we must eagerly fail requests to prevent
+                        // leaking tasks onto the runtime.
+                        .push(svc::LoadShed::layer()),
+                )
+                .push(http::insert::NewInsert::<Route, _>::layer())
+                .push(
+                    metrics
+                        .http_profile_route_actual
+                        .to_layer::<classify::Response, _, RouteParams<T>>(),
+                )
+                // Depending on whether or not the request can be
+                // retried, it may have one of two `Body` types. This
+                // layer unifies any `Body` type into `BoxBody`.
+                .push_on_service(http::BoxRequest::erased())
+                // Sets an optional retry policy.
+                .push(retry::layer(metrics.http_profile_route_retry.clone()))
+                // Sets an optional request timeout.
+                .push(http::NewTimeout::layer())
+                // Records per-route metrics.
+                .push(
+                    metrics
+                        .http_profile_route
+                        .to_layer::<classify::Response, _, RouteParams<T>>(),
+                )
+                // Add l5d-dst-canonical header to requests.
+                //
+                // TODO(ver) move this into the endpoint stack so that we can
+                // only set this on meshed connections.
+                .push(
+                    http::NewHeaderFromTarget::<CanonicalDstHeader, _, _>::layer_via(
+                        |rp: &Self| {
+                            let LogicalAddr(addr) = rp.addr.clone();
+                            CanonicalDstHeader(addr)
+                        },
+                    ),
+                )
+                // Sets the per-route response classifier as a request
+                // extension.
+                .push(classify::NewClassify::layer())
+                .push_on_service(http::BoxResponse::layer())
+                .arc_new_clone_http()
+                .into_inner()
+        })
+    }
+}
+
+impl<T> svc::Param<LogicalAddr> for RouteParams<T> {
+    fn param(&self) -> LogicalAddr {
+        self.addr.clone()
+    }
+}
+
+impl<T: Clone> svc::Param<Distribution<T>> for RouteParams<T> {
+    fn param(&self) -> Distribution<T> {
+        self.distribution.clone()
+    }
+}
+
+impl<T> svc::Param<Route> for RouteParams<T> {
+    fn param(&self) -> Route {
+        self.profile.clone()
+    }
+}
+
+impl<T> svc::Param<metrics::ProfileRouteLabels> for RouteParams<T> {
+    fn param(&self) -> metrics::ProfileRouteLabels {
+        metrics::ProfileRouteLabels::outbound(self.addr.clone(), &self.profile)
+    }
+}
+
+impl<T> svc::Param<http::ResponseTimeout> for RouteParams<T> {
+    fn param(&self) -> http::ResponseTimeout {
+        http::ResponseTimeout(self.profile.timeout())
+    }
+}
+
+impl<T> svc::Param<classify::Request> for RouteParams<T> {
+    fn param(&self) -> classify::Request {
+        self.profile.response_classes().clone().into()
+    }
+}
