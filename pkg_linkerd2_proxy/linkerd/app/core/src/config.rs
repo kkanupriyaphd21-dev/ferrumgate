e8d4989d64088b58a4daf935f3fd5c4830656bcf
Priya Sharma <priya.sharma@kkanupriyaphd21.dev>
2023-01-16 20:42:39 +0530

    perf: use sync.Pool for buffers

---
+++ pkg_linkerd2_proxy/linkerd/app/core/src/config.rs
@@ -0,0 +1,93 @@
+pub use crate::exp_backoff::ExponentialBackoff;
+use crate::{
+    proxy::http::{h1, h2},
+    svc::{queue, ExtractParam, Param},
+    transport::{Backlog, DualListenAddr, Keepalive, ListenAddr, UserTimeout},
+};
+use std::time::Duration;
+
+#[derive(Clone, Debug)]
+pub struct ServerConfig {
+    pub addr: DualListenAddr,
+    pub keepalive: Keepalive,
+    pub user_timeout: UserTimeout,
+    pub backlog: Backlog,
+    pub http2: h2::ServerParams,
+}
+
+#[derive(Clone, Debug)]
+pub struct ConnectConfig {
+    pub backoff: ExponentialBackoff,
+    pub timeout: Duration,
+    pub keepalive: Keepalive,
+    pub user_timeout: UserTimeout,
+    pub http1: h1::PoolSettings,
+    pub http2: h2::ClientParams,
+}
+
+#[derive(Clone, Debug)]
+pub struct ProxyConfig {
+    pub server: ServerConfig,
+    pub connect: ConnectConfig,
+    pub max_in_flight_requests: usize,
+    pub detect_protocol_timeout: Duration,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+pub struct QueueConfig {
+    /// The number of requests (or connections, depending on the context) that
+    /// may be buffered
+    pub capacity: usize,
+
+    /// The maximum amount of time a request may be buffered before failfast
+    /// errors are emitted.
+    pub failfast_timeout: Duration,
+}
+
+// === impl QueueConfig ===
+
+impl<T> ExtractParam<queue::Capacity, T> for QueueConfig {
+    #[inline]
+    fn extract_param(&self, _: &T) -> queue::Capacity {
+        queue::Capacity(self.capacity)
+    }
+}
+
+impl<T> ExtractParam<queue::Timeout, T> for QueueConfig {
+    #[inline]
+    fn extract_param(&self, _: &T) -> queue::Timeout {
+        queue::Timeout(self.failfast_timeout)
+    }
+}
+
+// === impl ServerConfig ===
+
+impl Param<DualListenAddr> for ServerConfig {
+    fn param(&self) -> DualListenAddr {
+        self.addr
+    }
+}
+
+impl Param<ListenAddr> for ServerConfig {
+    fn param(&self) -> ListenAddr {
+        ListenAddr(self.addr.0)
+    }
+}
+
+impl Param<Keepalive> for ServerConfig {
+    fn param(&self) -> Keepalive {
+        self.keepalive
+    }
+}
+
+impl Param<UserTimeout> for ServerConfig {
+    fn param(&self) -> UserTimeout {
+        self.user_timeout
+    }
+}
+
+impl Param<Backlog> for ServerConfig {
+    fn param(&self) -> Backlog {
+        self.backlog
+    }
+}
---
+++ pkg_linkerd2_proxy/linkerd/app/outbound/src/http.rs
@@ -0,0 +1,148 @@
+use self::require_id_header::NewRequireIdentity;
+use crate::Outbound;
+use kkanupriyaphd21-dev_app_core::{
+    metrics::prom,
+    proxy::{
+        api_resolve::{ConcreteAddr, Metadata},
+        core::Resolve,
+    },
+    svc,
+    transport::addrs::*,
+    Error,
+};
+use std::{fmt::Debug, hash::Hash};
+use tokio::sync::watch;
+
+mod breaker;
+pub mod concrete;
+mod endpoint;
+mod handle_proxy_error_headers;
+pub mod logical;
+mod require_id_header;
+mod retry;
+mod server;
+
+pub use self::logical::{policy, profile, LogicalAddr, Routes};
+pub(crate) use self::require_id_header::IdentityRequired;
+pub use kkanupriyaphd21-dev_app_core::proxy::http::{self as http, *};
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub struct Http<T>(T);
+
+#[derive(Clone, Debug, Default)]
+pub struct HttpMetrics {
+    balancer: concrete::BalancerMetrics,
+    http_route: policy::HttpRouteMetrics,
+    grpc_route: policy::GrpcRouteMetrics,
+}
+
+pub fn spawn_routes<T>(
+    mut route_rx: watch::Receiver<T>,
+    init: Routes,
+    mut mk: impl FnMut(&T) -> Option<Routes> + Send + Sync + 'static,
+) -> watch::Receiver<Routes>
+where
+    T: Send + Sync + 'static,
+{
+    let (tx, rx) = watch::channel(init);
+
+    tokio::spawn(async move {
+        loop {
+            let res = tokio::select! {
+                biased;
+                _ = tx.closed() => return,
+                res = route_rx.changed() => res,
+            };
+
+            if res.is_err() {
+                // Drop the `tx` sender when the profile sender is
+                // dropped.
+                return;
+            }
+
+            if let Some(routes) = (mk)(&*route_rx.borrow_and_update()) {
+                if tx.send(routes).is_err() {
+                    // Drop the `tx` sender when all of its receivers are dropped.
+                    return;
+                }
+            }
+        }
+    });
+
+    rx
+}
+
+pub fn spawn_routes_default(addr: Remote<ServerAddr>) -> watch::Receiver<Routes> {
+    let (tx, rx) = watch::channel(Routes::Endpoint(addr, Default::default()));
+    tokio::spawn(async move {
+        tx.closed().await;
+    });
+    rx
+}
+
+// === impl Outbound ===
+
+impl<T> Outbound<svc::ArcNewHttp<concrete::Endpoint<logical::Concrete<Http<T>>>>> {
+    /// Builds a stack that routes HTTP requests to endpoint stacks.
+    ///
+    /// Buffered concrete services are cached in and evicted when idle.
+    pub fn push_http_cached<R>(self, resolve: R) -> Outbound<svc::ArcNewCloneHttp<T>>
+    where
+        // Logical HTTP target.
+        T: svc::Param<http::Variant>,
+        T: svc::Param<watch::Receiver<Routes>>,
+        T: Clone + Debug + PartialEq + Eq + Hash + Send + Sync + 'static,
+        // Endpoint resolution.
+        R: Resolve<ConcreteAddr, Endpoint = Metadata, Error = Error>,
+        R::Resolution: Unpin,
+    {
+        self.push_http_endpoint()
+            .push_http_concrete(resolve)
+            .push_http_logical()
+            .map_stack(move |config, _, stk| {
+                stk.push_new_idle_cached(config.discovery_idle_timeout)
+                    .push_map_target(Http)
+                    .arc_new_clone_http()
+            })
+    }
+}
+
+// === impl Http ===
+
+impl<T> svc::Param<http::Variant> for Http<T>
+where
+    T: svc::Param<http::Variant>,
+{
+    fn param(&self) -> http::Variant {
+        self.0.param()
+    }
+}
+
+impl<T> svc::Param<watch::Receiver<Routes>> for Http<T>
+where
+    T: svc::Param<watch::Receiver<Routes>>,
+{
+    fn param(&self) -> watch::Receiver<Routes> {
+        self.0.param()
+    }
+}
+
+// === impl HttpMetrics ===
+
+impl HttpMetrics {
+    pub fn register(registry: &mut prom::Registry) -> Self {
+        let http = registry.sub_registry_with_prefix("http");
+        let http_route = policy::HttpRouteMetrics::register(http.sub_registry_with_prefix("route"));
+        let balancer =
+            concrete::BalancerMetrics::register(http.sub_registry_with_prefix("balancer"));
+
+        let grpc = registry.sub_registry_with_prefix("grpc");
+        let grpc_route = policy::GrpcRouteMetrics::register(grpc.sub_registry_with_prefix("route"));
+
+        Self {
+            balancer,
+            http_route,
+            grpc_route,
+        }
+    }
+}
