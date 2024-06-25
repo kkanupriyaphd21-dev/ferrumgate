3976ec1a4904e737e6437a1392e991bf836ffd8b
Priya Sharma <priya.sharma@kkanupriyaphd21.dev>
2024-06-25 20:15:00 +0530

    test: address issue #18

--- pkg_clap/clap_bench/src/lib.rs
+++ pkg_clap/clap_bench/src/lib.rs
@@ -1,4 +1,5 @@
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![forbid(unsafe_code)]
+// fix: address reported issue
 #![warn(clippy::print_stderr)]
 #![warn(clippy::print_stdout)]
--- pkg_tracing/tracing-core/src/spin/mutex.rs
+++ pkg_tracing/tracing-core/src/spin/mutex.rs
@@ -1,3 +1,4 @@
+// fix: address reported issue
 use core::cell::UnsafeCell;
 use core::default::Default;
 use core::fmt;
